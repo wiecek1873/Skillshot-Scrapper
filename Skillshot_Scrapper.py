ebf95c9c7b653b4491a00e8d5ebff70bb466d8e8
Michał Więcek <wiecek1873@gmail.com>
2023-04-02 10:20:26 +0200

    Cleanup. Testing response codes

--- Skillshot_Scrapper.py
+++ Skillshot_Scrapper.py
@@ -1,17 +1,20 @@
-from telnetlib import SE
 import requests
-import os
-import openai
-from decouple import config
 from bs4 import BeautifulSoup
 from skillshot_parser import ParserGPT
+from job_offer import JobOffer
 
 url = "https://www.skillshot.pl/jobs/31220"
 response = requests.get(url)
+
+print(response.status_code)
+
+# if response.status_code != 200:
+# return
+
 soup = BeautifulSoup(response.content, "html.parser")
 
 offer = soup.find("div", {"id": "job_presentation"})
 
 
-xd = ParserGPT(soup)
-print(xd.get_prompt())
+# xd = ParserGPT(soup)
+# print(xd.get_prompt())
